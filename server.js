b29560cb0c575b76c49905f509b2a5c7d1ab4762
abbasghori29 <132843772+abbasghori29@users.noreply.github.com>
2023-08-09 14:30:54 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import categoryRoutes from "./routes/categoryRoutes.js"
 import productRoutes from "./routes/productRoutes.js"
 import cors from 'cors';
 import path from 'path';
-
+import {fileURLToPath} from 'url'; // this is used because we are using module method to import 
 //configure .env
 dotenv.config();
 
@@ -17,7 +17,9 @@ connectDB();
 // rest objects
 const app = express()
 
-
+//esModuleErrorFix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //middlewares
 app.use(cors())
 app.use(express.json())
